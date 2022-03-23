5c20e6fdea87f911fb85e452b09b1cce09980b77
iuliaCiof <iulia.ciofindac@student.upt.ro>
2022-03-23 18:55:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
 
 int main() {
-    std::cout << "Hello Easy C++ project!hii" << std::endl;
+    enum Cifre{I=1, V=5, X=10, L=50, C=100, D=500, M=1000};
+    int N;
+    int N_arr[7];
+
+    cout<<"Dati N: ";   cin>>N;
+    int i=0;
+    N_arr[i]=N%10;
+    i++;
+    while(N_arr[i]<N){
+        int s=pow(10,i+1);
+        N_arr[i]=floor(N%s);
+        i++;
+    }
+
+    for(i=0;i<7;i++){
+        cout<<" "<<N_arr[i];
+    }
 }
